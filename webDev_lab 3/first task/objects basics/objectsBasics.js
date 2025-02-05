8ef0df4ad34d6d69abee1a9bba75e1763cd623c9
Yertay <zhambulaie@gmail.com>
2025-02-05 20:04:41 +0600

    first task

--- webDev_lab 3/first task/objects basics/objectsBasics.js
+++ webDev_lab 3/first task/objects basics/objectsBasics.js
@@ -60,4 +60,104 @@ menu = {
     title: "My menu"
 };
 
-//oject reference and copying
+//object methods
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this
+  };
+}
+
+let user = makeUser();
+
+alert( user.ref.name ); // output: error
+
+//calculator
+let calculator = {
+sum() {
+    return this.a + this.b;
+},
+
+mul() {
+    return this.a * this.b;
+},
+
+read() {
+  this.a = +prompt('a?', 0);
+  this.b = +prompt('b?', 0);
+  }
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+
+//ladder
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    alert( this.step );
+    return this;
+  }
+};
+
+ladder.up().up().down().showStep().down().showStep(); 
+
+//constructor
+
+let obj = {};
+
+function A() { return obj; }
+function B() { return obj; }
+
+alert( new A() == new B() ); // true
+
+//new calculator
+
+function Calculator() {
+
+  this.read = function() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  };
+
+  this.sum = function() {
+    return this.a + this.b;
+  };
+
+  this.mul = function() {
+    return this.a * this.b;
+  };
+}
+
+let calc = new Calculator();
+calc.read();
+
+alert( "Sum=" + calc.sum() );
+alert( "Mul=" + calc.mul() );
+
+//new accumulator
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+    this.value += +prompt('How much to add?', 0);
+  };
+
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
+
+
